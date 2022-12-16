d09fc1c601c6018ea164fb37e4ca72e86ee7a4ed
Erika-P <119851437+Erika-P@users.noreply.github.com>
2022-12-16 02:35:48 +0100

    Update CreerCompte.java

--- Encheres/src/main/java/fr/eni/encheres/servlets/CreerCompte.java
+++ Encheres/src/main/java/fr/eni/encheres/servlets/CreerCompte.java
@@ -38,7 +38,13 @@ public class CreerCompte extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		    String pseudo       = request.getParameter("pseudo");
+		
+	RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/register.jsp");
+	       
+		Utilisateur user = null;
+		
+		try {  
+		String pseudo       = request.getParameter("pseudo");
 	        String nom          = request.getParameter("nom");
 	        String prenom       = request.getParameter("prenom");
 	        String email        = request.getParameter("email");
@@ -52,37 +58,76 @@ public class CreerCompte extends HttpServlet {
 	        String administration      = request.getParameter("mdpConf");
 
 
-	        Utilisateur ajoutUtilisateur = new Utilisateur(pseudo,nom,prenom,email,telephone,rue,codePostal,ville,mdp);
-	        UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
-	        Utilisateur utilisateur = null;
-	        RequestDispatcher dispatcher = null;
-	        String messageErreur = "" ;
+			List<String> allPseudos = UtilisateurManager.selectAllPseudos();
 
-	        if (!mdpConf.equals(mdp)){
-
-	            request.setAttribute("ajoutUtilisateur", ajoutUtilisateur);
-	            dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/register.jsp");
-	            dispatcher.forward(request, response);}
-	            else{
-	                try {
-	                    utilisateur = utilisateurManager.insert(ajoutUtilisateur);
-
-	                } catch (Exception e) {
-	                    // Je récupère le message de l'exception
-	                    messageErreur = e.getMessage();
-	                }
-
-	                if (utilisateur == null) {
-	                    request.setAttribute("Erreur", messageErreur);
-	                    request.setAttribute("ajoutUtilisateur", ajoutUtilisateur);
-	                    dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/register.jsp");
-	                    dispatcher.forward(request, response);
-	                } else {
-	                    HttpSession session = request.getSession();
-	                    session.setAttribute("utilisateur", utilisateur);
-
-	                    this.getServletContext().getRequestDispatcher("/Accueil").forward(request, response);
-	                }
-	            }
+			
+			if (pseudo.length()==0 || pseudo.isEmpty() ) {
+				
+				request.setAttribute("erreur", "Le pseudo n'a pas été renseigné, veuillez le saisir ...");			
+				dispatcher.forward(request, response);
+			}
+			else if (allPseudos.contains(pseudo)) {
+				request.setAttribute("erreur", "Ce pseudo est déjà utilisé, veuillez le saisir ...");
+									
+				dispatcher.forward(request, response);
+			}
+			else if(nom.length()==0 || nom.isEmpty() ) {
+				request.setAttribute("erreur", "Le nom n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}	
+			else if (prenom.length()==0 || prenom.isEmpty() ) {
+				request.setAttribute("erreur", "Le pr�nom n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (email.length()==0 || email.isEmpty() ) {
+				request.setAttribute("erreur", "L'email n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (telephone.length()==0 || telephone.isEmpty() ) {
+				request.setAttribute("erreur", "Le mot de passe n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (rue.length()==0 || rue.isEmpty() ) {
+				request.setAttribute("erreur", "La rue n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (codePostal.length()==0 || codePostal.isEmpty() ) {
+				request.setAttribute("erreur", "Le code postal n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (ville.length()==0 || ville.isEmpty() ) {
+				request.setAttribute("erreur", "La ville n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (mdp.length()==0 || mdp.isEmpty() ) {
+				request.setAttribute("erreur", "Le mot de passe n'a pas été renseigné, veuillez le saisir ...");
+				dispatcher.forward(request, response);
+			}
+			else if (mdpConf.equals(mdp)) {
+			
+				user = new Utilisateur(pseudo,nom,prenom,email,telephone,rue,codePostal,ville,mdp);
+				user = UtilisateurManager.inscriptionUtilisateur(user);
+				
+				if (user != null) {
+					HttpSession session = request.getSession();
+					session.setAttribute("ConnectedUser", user);
+					
+					this.getServletContext().getRequestDispatcher("/Accueil").forward(request, response);
+				} 
+				else 
+				{
+					request.setAttribute("erreur", "Aucun utilisateur");
+					dispatcher.forward(request, response);
+				}
+	
+			}
+			
+		}catch (BusinessException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (DalException e) {
+			e.printStackTrace();
+		}	
 	}
 }
